22456715585db06eb6170982a7b2302041509a7d
wing <1531780174@qq.com>
2022-01-22 21:29:56 +0800

    update html

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 <template>
   <img alt="Vue logo"
        src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue33311 + Vite" />
+  <HelloWorld msg="Hello Vue33311asdf + Vite" />
 </template>
 
 <style>
